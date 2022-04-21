57867cc0e6617d3064857ae099e0a2cb329c0029
Abidur Rouf <arouf1471@gmail.com>
2022-04-21 23:18:11 +0100

    new

--- pages/ComputerScience.jsx
+++ pages/ComputerScience.jsx
@@ -5,6 +5,9 @@ import { db } from '../firebase';
 import Optiondescription from '../components/classes/Optiondescription';
 import { PlusCircleIcon } from "@heroicons/react/outline";
 import Modal2 from '../components/Modal2';
+import { signIn, useSession } from 'next-auth/react';
+
+
 
 let isModalOpen = false
 
@@ -15,42 +18,49 @@ function toggleModal2() {
   }
   isModalOpen = true
   document.body.classList += " modal--open"
-} 
+}
 
 function ComputerScience() {
-
+  const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
 
   useEffect(
     () =>
       onSnapshot(
-        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        query(collection(db, "reviews"), orderBy("timestamp", "desc")),
         (snapshot) => {
           setPosts(snapshot.docs);
         }
       ),
     [db]
-    );
-
-    console.log(posts)
+  );
 
   return (
     <div className="option__container">
+
       <Optiondescription
         title="Computer Science"
         description="Computer science (CS) is the study of computers and algorithmic processes, 
-        including their principles, their hardware and software designs,
-        their applications, and their impact on society."
+          including their principles, their hardware and software designs,
+          their applications, and their impact on society."
       />
 
       <Modal2 toggleModal={toggleModal2} />
 
-      <div className="add__review">
-        <PlusCircleIcon
-          className='post__icon click'
-          onClick={() => toggleModal2()}
-        />
+        {session ? (
+          <div className="add__review">
+            <PlusCircleIcon
+              className='post__icon click'
+              onClick={() => toggleModal2()}
+            />
+          </div>
+        ) : (
+          <div className="option__signin">
+          <button
+            className='signin btn'
+            onClick={signIn}>Sign In To Post</button>
       </div>
+        )}
 
 
       {posts.map((post) => (
@@ -59,7 +69,6 @@ function ComputerScience() {
           id={post.id}
           username={post.data().username}
           userImg={post.data().profileImg}
-          // img={post.data().image}
           caption={post.data().caption}
         />
       ))}
